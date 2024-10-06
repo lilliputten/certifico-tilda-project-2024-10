45ede6186f195eae71c51e6e10426af4afe2a25e
lilliputten <lilliputten@gmail.com>
2024-10-06 22:53:44 +0300

    Issue #0: Basic project configuration and minimal js & css source files.

---
+++ README.md
@@ -0,0 +1,14 @@
+<!--
+@since 2024.10.06, 22:56
+@changed 2024.10.06, 22:56
+-->
+
+# Certifico tilda site compiling assets
+
+TODO: Describe workflow here.
+
+- Version: 0.0.0
+- Last changes timestamp: 2024.07.25 21:32 +0500
+
+TODO: Add the project description.
+
---
+++ src/scripts/root.ts
@@ -0,0 +1,11 @@
+/**!
+ * @module Scripts root module
+ * @since 2024.10.06, 22:40
+ * @changed 2024.10.06, 22:40
+ */
+
+import appInfo from '../app-info.json';
+// const { projectName, version, timestamp } = appInfo;
+console.log('App started', appInfo);
+
+debugger;
