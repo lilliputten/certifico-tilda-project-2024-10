69a296fa42b28d8faa6fc365f66af6a0db44cb9b
lilliputten <lilliputten@gmail.com>
2024-10-07 00:54:19 +0300

    Build publish.0.0.1, 2024.10.07 00:51 +0300 (241007-0051)

---
+++ scripts.js
@@ -0,0 +1,8 @@
+(()=>{"use strict";(()=>{const t=JSON.parse('{"projectName":"vanilla-tasks","timestamp":"2024.07.25 15:40 +0000","timetag":"240725-1540","version":"0.0.13","currentTimeStr":"2024.07.25 15:40 +0000","currentTimeTag":"240725-1540","gitCommitHash":"dfc7f6e","gitBranch":"main"}');
+/**!
+ * @module Scripts root module
+ * @since 2024.10.06, 22:40
+ * @changed 2024.10.07, 00:41
+ */
+console.log("App started",t);debugger})()})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0cy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFPQUEsUUFBUUMsSUFBSSxjQUFlLEdBRTNCLFEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jZXJ0aWZpY28taW5jbHVkZWQtYXNzZXRzLy4vc3JjL3NjcmlwdHMvcm9vdC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyIvKiohXG4gKiBAbW9kdWxlIFNjcmlwdHMgcm9vdCBtb2R1bGVcbiAqIEBzaW5jZSAyMDI0LjEwLjA2LCAyMjo0MFxuICogQGNoYW5nZWQgMjAyNC4xMC4wNywgMDA6NDFcbiAqL1xuXG5pbXBvcnQgYXBwSW5mbyBmcm9tICcuLi9hcHAtaW5mby5qc29uJztcbmNvbnNvbGUubG9nKCdBcHAgc3RhcnRlZCcsIGFwcEluZm8pO1xuXG5kZWJ1Z2dlcjtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
