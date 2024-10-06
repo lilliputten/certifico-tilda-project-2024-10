e12aa9c86a526a8ae95488c012197ce4bd8b6bec
lilliputten <lilliputten@gmail.com>
2024-10-07 01:36:38 +0300

    Build publish.0.0.1, 2024.10.07 01:35 +0300 (241007-0135)

--- scripts.js
+++ scripts.js
@@ -1,8 +1,2 @@
-(()=>{"use strict";(()=>{const t=JSON.parse('{"projectName":"vanilla-tasks","timestamp":"2024.07.25 15:40 +0000","timetag":"240725-1540","version":"0.0.13","currentTimeStr":"2024.07.25 15:40 +0000","currentTimeTag":"240725-1540","gitCommitHash":"dfc7f6e","gitBranch":"main"}');
-/**!
- * @module Scripts root module
- * @since 2024.10.06, 22:40
- * @changed 2024.10.07, 00:41
- */
-console.log("App started",t);debugger})()})();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0cy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFPQUEsUUFBUUMsSUFBSSxjQUFlLEdBRTNCLFEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jZXJ0aWZpY28taW5jbHVkZWQtYXNzZXRzLy4vc3JjL3NjcmlwdHMvcm9vdC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyIvKiohXG4gKiBAbW9kdWxlIFNjcmlwdHMgcm9vdCBtb2R1bGVcbiAqIEBzaW5jZSAyMDI0LjEwLjA2LCAyMjo0MFxuICogQGNoYW5nZWQgMjAyNC4xMC4wNywgMDA6NDFcbiAqL1xuXG5pbXBvcnQgYXBwSW5mbyBmcm9tICcuLi9hcHAtaW5mby5qc29uJztcbmNvbnNvbGUubG9nKCdBcHAgc3RhcnRlZCcsIGFwcEluZm8pO1xuXG5kZWJ1Z2dlcjtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
+(()=>{"use strict";(()=>{const e=JSON.parse('{"kr":"certifico-included-assets v.0.0.1 / 2024.10.07 01:35 +0300"}');console.log("App started",e.kr);debugger})()})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0cy5qcyIsIm1hcHBpbmdzIjoibUhBT0FBLFFBQVFDLElBQUksY0FBZSxNQUMzQixRIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY2VydGlmaWNvLWluY2x1ZGVkLWFzc2V0cy8uL3NyYy9zY3JpcHRzL3Jvb3QudHMiXSwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBAbW9kdWxlIFNjcmlwdHMgcm9vdCBtb2R1bGVcbiAqIEBzaW5jZSAyMDI0LjEwLjA2LCAyMjo0MFxuICogQGNoYW5nZWQgMjAyNC4xMC4wNywgMDE6MzNcbiAqL1xuXG5pbXBvcnQgeyBhcHBWZXJzaW9uSGFzaCB9IGZyb20gJy4uL2FwcC1pbmZvLmpzb24nO1xuY29uc29sZS5sb2coJ0FwcCBzdGFydGVkJywgYXBwVmVyc2lvbkhhc2gpO1xuZGVidWdnZXI7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
