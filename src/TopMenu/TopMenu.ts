c429f744ab61a2c3a706f59aa300db3fa4a67019
lilliputten <lilliputten@gmail.com>
2024-10-08 21:46:48 +0300

    Issue #1: Implemented universal sub menu (copied into nav header to appear in mobile panel).

--- src/TopMenu/TopMenu.ts
+++ src/TopMenu/TopMenu.ts
@@ -1,12 +1,24 @@
-import { blockTopMenu } from '../variables';
-
-const BLOCK = blockTopMenu;
+import { blockTopMenu, blockNavHeader } from '../variables';
 
 import './TopMenu.styles.scss';
 
 export function initTopMenu() {
-  const selectorNavHeader = `.uc-NavHeader ${BLOCK}__adress-container .t-descr`;
-  const selectorBlock = `.uc-TopMenu`;
-  console.log('[TopMenu:initTopMenu]', {});
-  debugger;
+  // Find and move menu to the nav header...
+  const menuSelector = `.uc-TopMenu ${blockTopMenu}__maincontainer`;
+  const navHeaderWrapperSelector = `.uc-NavHeader ${blockNavHeader}`;
+  const menuNode = document.querySelector(menuSelector);
+  const navHeaderWrapperNode = document.querySelector(navHeaderWrapperSelector);
+  // Clone menu node...
+  const menuNodeCopy = menuNode.cloneNode(true) as Element;
+  // Clean styles...
+  menuNodeCopy.removeAttribute('style');
+  menuNodeCopy.classList.toggle('SubMenu', true);
+  // Add menu node copy to the hav header...
+  navHeaderWrapperNode.append(menuNodeCopy);
+  // const selectorNavHeader = `.uc-NavHeader ${BLOCK}__adress-container .t-descr`;
+  console.log('[TopMenu:initTopMenu]', {
+    menuNodeCopy,
+    menuNode,
+    navHeaderWrapperNode,
+  });
 }
