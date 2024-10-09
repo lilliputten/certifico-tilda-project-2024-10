6671d27a50f8d2bcd498d81713d3d82192f31d7a
lilliputten <lilliputten@gmail.com>
2024-10-10 01:30:22 +0300

    Issue #3: Fixed minor issues on `Reasons` and `RequestForm` blocks.

--- src/misc-styles/index.ts
+++ src/misc-styles/index.ts
@@ -1,6 +1,7 @@
 import './DevWarning.scss';
 import './HeroMenu.scss';
 import './Reasons.scss';
+import './RequestForm.scss';
 import './SocialIcon.scss';
 import './TopHero.scss';
 import './Triggers.scss';
